f491712a7f96eb67debd02f644ff9e04c94e34c2
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-02-13 17:41:48 +0100

    Extend order service to get all orders with specific id

--- Service/OrderService.php
+++ Service/OrderService.php
@@ -1,37 +1,53 @@
 <?php
 
- declare(strict_types=1);
+declare(strict_types=1);
 
- namespace YellowCard\ProductsExporter\Service;
+namespace YellowCard\ProductsExporter\Service;
 
- use Magento\Sales\Api\Data\OrderInterface;
- use Magento\Sales\Model\ResourceModel\Order\Collection;
- use Magento\Sales\Model\ResourceModel\Order\CollectionFactory;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\ResourceModel\Order\Collection;
+use Magento\Sales\Model\ResourceModel\Order\CollectionFactory;
 
- class OrderService
- {
+class OrderService
+{
     /**
-     *
      * @param CollectionFactory $collectionFactory
-     * @param StatusService $statusService
+     * @param StatusService     $statusService
      */
-     public function __construct(
-         private CollectionFactory $collectionFactory,
-         private StatusService $statusService
-     ) {
-     }
+    public function __construct(
+        private CollectionFactory $collectionFactory,
+        private StatusService $statusService,
+        private OrderRepositoryInterface $orderRepository,
+    ) {
+    }
 
-     /**
-      * Returns collection of orders with specific status provided in config by user
-      *
-      * @return string
-      */
-     public function getOrders(): Collection
-     {
-         $collection = $this->collectionFactory->create();
-         $collection->addFieldToFilter('status', $this->statusService->getStatus());
-         $collection->addAttributeToSelect(OrderInterface::INCREMENT_ID);
+    /**
+     * Returns collection of orders IDS with specific status provided in config by user
+     *
+     * @return Collection
+     */
+    public function getOrderIds(): Collection
+    {
+        $collection = $this->collectionFactory->create();
+        $collection->addFieldToFilter('status', $this->statusService->getStatus());
+        $collection->addAttributeToSelect(OrderInterface::INCREMENT_ID);
 
-         return $collection;
-     }
- }
+        return $collection;
+    }
+
+    /**
+     * Loads all orders with specific status, found by ids provided from method above
+     *
+     * @return array
+     */
+    public function getOrders(): array
+    {
+        $orders = [];
+        foreach ($this->getOrderIds() as $orderId) {
+            $orders[] = $this->orderRepository->get($orderId->getIncrementId());
+        }
+
+        return $orders;
+    }
+}
