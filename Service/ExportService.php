fc5d0b4ebba76409a09828030a3048a259122783
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-02-19 22:51:29 +0100

    Add try catch block to createExport method

--- Service/ExportService.php
+++ Service/ExportService.php
@@ -1,31 +1,39 @@
 <?php
 
- declare(strict_types=1);
+declare(strict_types=1);
 
- namespace YellowCard\ProductsExporter\Service;
+namespace YellowCard\ProductsExporter\Service;
 
- class ExportService
- {
-     /**
-      * @param ProductService $productService
-      */
-     public function __construct(private ProductService $productService)
-     {
-     }
+use Psr\Log\LoggerInterface;
 
-     /**
-      * Shows on the screen purchases products from provided orders. Sku, quantity and order Id
-      *
-      * @return void
-      */
-     public function createExport(): void
-     {
-         foreach ($this->productService->getProducts() as $product) {
-             foreach ($product as $item) {
-                 echo $item->getSku()
-                     . " " . number_format((float)$item->getQtyOrdered(), 2)
-                     . " " . $item->getOrderId() . '</br>';
-             }
-         }
-     }
- }
+class ExportService
+{
+    /**
+     * @param ProductService  $productService
+     * @param LoggerInterface $logger
+     */
+    public function __construct(private ProductService $productService, private LoggerInterface $logger)
+    {
+    }
+
+    /**
+     * Shows on the screen purchases products from provided orders. Sku, quantity and order Id
+     *
+     * @return void
+     */
+    public function createExport(): void
+    {
+        try {
+            foreach ($this->productService->getProducts() as $product) {
+                foreach ($product as $item) {
+                    echo $item->getSku()
+                        . " " . number_format((float)$item->getQtyOrdered(), 2)
+                        . " " . $item->getOrderId() . '</br>';
+                }
+            }
+        } catch (\Exception $exception) {
+            $this->logger->critical($exception->getMessage());
+        }
+
+    }
+}
