c27ebf79d6e440f502437338d453ac6a9aa9fa03
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-04 20:17:17 +0100

    Add logger to  ExportedOrders repository model

--- Model/ExportedOrdersRepository.php
+++ Model/ExportedOrdersRepository.php
@@ -7,18 +7,24 @@ namespace YellowCard\ProductsExporter\Model;
 use Psr\Log\LoggerInterface;
 use YellowCard\ProductsExporter\Api\Data\ExportedOrdersInterface;
 use YellowCard\ProductsExporter\Api\ExportedOrdersRepositoryInterface;
+use YellowCard\ProductsExporter\Enum\LoggerMessages;
 use YellowCard\ProductsExporter\Model\ResourceModel\ExportedOrders as ExportedOrdersResource;
 
 class ExportedOrdersRepository implements ExportedOrdersRepositoryInterface
 {
     public function __construct(
-        private ExportedOrdersResource $exportedOrdersResource
+        private ExportedOrdersResource $exportedOrdersResource,
+        private LoggerInterface $logger
     ) {
     }
 
     public function save(ExportedOrdersInterface $exportedOrders
     ): ExportedOrdersInterface {
-        $this->exportedOrdersResource->save($exportedOrders);
+        try{
+            $this->exportedOrdersResource->save($exportedOrders);
+        } catch (\Exception $exception) {
+            $this->logger->critical(LoggerMessages::DB_FAILED->value. " : " .$exception->getMessage());
+        }
 
         return $exportedOrders;
     }
