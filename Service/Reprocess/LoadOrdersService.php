1be6a0dfbb103f215e80be9a76ac7fd25f2f62a1
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-21 21:18:28 +0100

    Add doc-blocks

--- Service/Reprocess/LoadOrdersService.php
+++ Service/Reprocess/LoadOrdersService.php
@@ -6,11 +6,21 @@ use Magento\Sales\Api\OrderRepositoryInterface;
 
 class LoadOrdersService
 {
+     /**
+     * @param OrderRepositoryInterface $orderRepository
+     */
     public function __construct(
         private readonly OrderRepositoryInterface $orderRepository
     ) {
     }
 
+     /**
+     * Return specified orders from database filtered by specified numbers
+     *
+     * @param array $raport
+     *
+     * @return array
+     */
     public function execute(array $raport)
     {
         $orderIds = $this->getOrderIdsFromRaport($raport);
@@ -19,6 +29,13 @@ class LoadOrdersService
         return $orders;
     }
 
+    /**
+     * Return array of strings with order numbers from string taken from provided raport
+     *
+     * @param array $raport
+     *
+     * @return array
+     */
     private function getOrderIdsFromRaport(array $raport): array
     {
         $orderIds = explode(',', $raport['orders']);
@@ -26,6 +43,13 @@ class LoadOrdersService
         return $orderIds;
     }
 
+     /**
+     * Load all orders from provided array with Ids
+     *
+     * @param array $orderIds
+     *
+     * @return array
+     */
     private function loadOrders(array $orderIds): array
     {
         $orders = [];
