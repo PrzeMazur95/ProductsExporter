5f3d77d525e8a52036de5aab23cd099df09c4319
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-11 22:48:24 +0200

    Get download action to private method, grab it in try catch blocks

--- Controller/Adminhtml/Download/Download.php
+++ Controller/Adminhtml/Download/Download.php
@@ -2,59 +2,81 @@
 
 declare(strict_types=1);
 
- namespace YellowCard\ProductsExporter\Controller\Adminhtml\Download;
+namespace YellowCard\ProductsExporter\Controller\Adminhtml\Download;
 
- use Exception;
- use Magento\Backend\App\Action;
- use Magento\Backend\App\Action\Context;
- use Magento\Framework\App\Action\HttpGetActionInterface;
- use Magento\Framework\App\Config\ScopeConfigInterface;
- use Magento\Framework\App\Filesystem\DirectoryList;
- use Magento\Framework\App\Response\Http\FileFactory;
+use Exception;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Backend\Model\UrlInterface;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\Response\Http\FileFactory;
+use Magento\Framework\App\ResponseInterface;
 
- class Download extends Action implements HttpGetActionInterface
- {
-     private const DOWNLOAD_PATH = 'exportedProducts/';
-     private const EXTENSION = '.csv';
+class Download extends Action implements HttpGetActionInterface
+{
+    private const DOWNLOAD_PATH = 'exportedProducts/';
+    private const EXTENSION = '.csv';
 
-     public function __construct(
-         private readonly FileFactory $fileFactory,
-         private readonly ScopeConfigInterface $scopeConfig,
-         Context $context
-     ) {
-         parent::__construct($context);
-     }
+    public function __construct(
+        private readonly FileFactory $fileFactory,
+        private readonly ScopeConfigInterface $scopeConfig,
+        private readonly UrlInterface $url,
+        private readonly ResponseInterface $response,
+        Context $context
+    ) {
+        parent::__construct($context);
+    }
 
-     /**
-      * @throws Exception
-      */
-     public function execute()
-     {
-         $title =  $this->getRequest()->getParam('title');
-         $absoluteFilePath = self::DOWNLOAD_PATH.$title.self::EXTENSION;
+    /**
+     * @throws Exception
+     */
+    public function execute()
+    {
+        $this->downloadFile();
 
-         if(!file_exists($absoluteFilePath)) {
-             throw new Exception('not found, to be modified');
-         }
-         //later set this rm as a config value - if admin wants to delete downloaded file form server after it
-         $this->fileFactory->create(
-             $title.self::EXTENSION,
-             [
-                 'type' => 'filename',
-                 'value' => $absoluteFilePath,
-                 'rm' => $this->ifFileShouldBeDeleted()
-             ],
-             DirectoryList::PUB
-         );
-     }
 
-     /**
-      * Get from configuration, if admin wants to delete file after download or no
-      *
-      * @return bool
-      */
-      private function ifFileShouldBeDeleted(): bool
-      {
-         return (bool)$this->scopeConfig->getValue('yellowcard/general/deleteExportFile');
-      }
- }
+//        //should without redirect render message, that file was downloaded, or there is no file
+        //redirect couses no download, not message
+//        $url = $this->url->getUrl('productsexporter/index/index');
+//        $this->response->setRedirect($url);
+//
+//        return $this->response;
+    }
+
+    /**
+     * Get from configuration, if admin wants to delete file after download or no
+     *
+     * @return bool
+     */
+    private function ifFileShouldBeDeleted(): bool
+    {
+       return (bool)$this->scopeConfig->getValue('yellowcard/general/deleteExportFile');
+    }
+
+    private function downloadFile()
+    {
+        $title =  $this->getRequest()->getParam('title');
+        $absoluteFilePath = self::DOWNLOAD_PATH.$title.self::EXTENSION;
+
+        try{
+            if(!file_exists($absoluteFilePath)) {
+                $notFoundMsg = __('File does not exists, reproces your raport')->render();
+            }
+            $this->fileFactory->create(
+                $title.self::EXTENSION,
+                [
+                    'type' => 'filename',
+                    'value' => $absoluteFilePath,
+                    'rm' => $this->ifFileShouldBeDeleted()
+                ],
+                DirectoryList::PUB
+            );
+            $msg = __('File was successfully downloaded')->render();
+            $this->messageManager->addSuccessMessage($msg);
+        } catch (Exception $e) {
+            $this->messageManager->addErrorMessage(__('Something went wrong: %1', $e->getMessage()));
+        }
+    }
+}
